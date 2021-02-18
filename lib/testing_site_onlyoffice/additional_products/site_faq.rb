06708a35f45c357db7e46c0db57455543bb1fe29
Olga Nesterova <olganesterova034@gmail.com>
2021-02-18 13:30:39 +0300

    Add missed module TestingSiteOnlyoffice to class SiteFaq

--- lib/testing_site_onlyoffice/additional_products/site_faq.rb
+++ lib/testing_site_onlyoffice/additional_products/site_faq.rb
@@ -2,18 +2,20 @@
 
 # https://user-images.githubusercontent.com/40513035/103107863-93c01100-4653-11eb-9931-86b122a0d680.png
 
-class SiteFaq
-  include PageObject
+module TestingSiteOnlyoffice
+  class SiteFaq
+    include PageObject
 
-  div(:faq_indicator, xpath: '//div[@class="faqpage general_faq"]')
+    div(:faq_indicator, xpath: '//div[@class="faqpage general_faq"]')
 
-  def initialize(instance)
-    super(instance.webdriver.driver)
-    @instance = instance
-    wait_to_load
-  end
+    def initialize(instance)
+      super(instance.webdriver.driver)
+      @instance = instance
+      wait_to_load
+    end
 
-  def wait_to_load
-    @instance.webdriver.wait_until { faq_indicator_element.present? }
+    def wait_to_load
+      @instance.webdriver.wait_until { faq_indicator_element.present? }
+    end
   end
 end
