0c0f99877331d188b9e5c41b3c63690590eaaa69
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2021-02-19 16:38:12 +0300

    Add class SiteVersionHelper (#31)
    * Add class SiteVersionHelper
    
    * Add Error
    
    * undo
    
    * add full_site_version method
    
    * Review changes
    
    * Delete time in error
    
    * Return 'unknown' if error

---
+++ lib/testing_site_onlyoffice/site_version_helper.rb
@@ -0,0 +1,33 @@
+module TestingSiteOnlyoffice
+  class SiteVersionHelper
+    # @return [String] url of site
+    def self.site_url
+      return 'https://teamlab.info' if StaticDataTeamLab.portal_type == '.info'
+      return 'https://www.onlyoffice.com' if StaticDataTeamLab.portal_type == '.com'
+    end
+
+    def self.fetch_uri
+      URI("#{site_url}/revision?Site_Testing=4testing")
+    end
+
+    # @return [String] hash of site by url `site/revision`
+    def self.fetch_site_hash
+      Net::HTTP.get(fetch_uri).split[0]
+    rescue StandardError => e
+      OnlyofficeLoggerHelper.log("Cannot get site hash because of #{e}")
+      'unknown'
+    end
+
+    # @return [String] branch number of site by url `site/revision`
+    def self.fetch_site_branch
+      Net::HTTP.get(fetch_uri).strip.split('/')[-1]
+    rescue StandardError => e
+      OnlyofficeLoggerHelper.log("Cannot get site branch number because of #{e}")
+      'unknown'
+    end
+
+    def self.full_site_version
+      "branch: #{fetch_site_branch}, hash: #{fetch_site_hash}"
+    end
+  end
+end
