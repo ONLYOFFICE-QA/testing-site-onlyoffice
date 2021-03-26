1cb73124f14a99b052ee278c8ad40823107f88f3
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2021-03-26 01:08:29 -0700

    TestingSiteOnlyoffice module wrapping + commercial test renaming (#119)

--- shared_examples/commercial_installer_download.rb
+++ shared_examples/commercial_installer_download.rb
@@ -7,7 +7,7 @@ shared_examples_for 'commercial_installer_download' do |product, installers_list
 
       it "[Site][DownloadCommercial][#{product}] install link for `#{installer}` alive /download-commercial.aspx" do
         @current_installation.click_install_button
-        marketplace_title = SiteDownloadData.commercial_info[product.downcase][installer.to_s]['download']
+        marketplace_title = TestingSiteOnlyoffice::SiteDownloadData.commercial_info[product.downcase][installer.to_s]['download']
         expect(installers_download_page.check_opened_page_title).to eq(marketplace_title)
       end
     end
@@ -19,7 +19,7 @@ shared_examples_for 'commercial_installer_download' do |product, installers_list
         @current_installation = installers_download_page.get_blocks_by_product(product, installer)
       end
 
-      it "[Site][DownloadCommercial][EnterpriseDocs] buy link for `#{installer}` alive /download-commercial.aspx" do
+      it "[Site][DownloadCommercial][#{product}] buy link for `#{installer}` alive /download-commercial.aspx" do
         pricing_page = @current_installation.click_buy_button
         expect(pricing_page).to be_a(installers_download_page.pricing_page_by_product(product))
       end
@@ -39,7 +39,7 @@ shared_examples_for 'commercial_installer_download' do |product, installers_list
 
       it "[Site][DownloadCommercial][#{product}] instruction link for `#{installer}` alive /download.aspx" do
         installers_download_page.click_constructor_link(@current_installation.instruction_xpath)
-        instruction_title = SiteDownloadData.commercial_info[product.downcase][installer.to_s]['instruction']
+        instruction_title = TestingSiteOnlyoffice::SiteDownloadData.commercial_info[product.downcase][installer.to_s]['instruction']
         expect(installers_download_page.check_opened_page_title).to eq(instruction_title)
       end
     end
