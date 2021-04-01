e6d708f66b140294af5a830b9d8f7662ba349c15
Pavel Lobashov <shockwavenn@gmail.com>
2021-04-01 14:55:03 +0300

    Fix filename in TeamLab project
    Changed in https://github.com/ONLYOFFICE/testing-onlyoffice/pull/2525

--- lib/testing_site_onlyoffice.rb
+++ lib/testing_site_onlyoffice.rb
@@ -2,7 +2,7 @@
 
 require 'onlyoffice_webdriver_wrapper'
 require 'onlyoffice_gmail_helper'
-require_relative '../../TeamLab/Framework/TestInstance'
+require_relative '../../TeamLab/framework/test_instance'
 require_relative 'testing_site_onlyoffice/config'
 require_relative 'testing_site_onlyoffice/site_test_instance'
 require_relative 'testing_site_onlyoffice/site_home_page'
