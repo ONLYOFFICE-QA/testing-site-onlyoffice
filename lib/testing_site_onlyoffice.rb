158ebfeb046f606d4825d2323106c2236f400678
Olga Nesterova <olganesterova034@gmail.com>
2021-04-13 23:42:33 -0700

    Gems changes

--- lib/testing_site_onlyoffice.rb
+++ lib/testing_site_onlyoffice.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require 'onlyoffice_webdriver_wrapper'
-require 'onlyoffice_gmail_helper'
-# require_relative '../../TeamLab/framework/test_instance'
 require_relative 'testing_site_onlyoffice/config'
 require_relative 'testing_site_onlyoffice/site_test_instance'
 require_relative 'testing_site_onlyoffice/site_home_page'
