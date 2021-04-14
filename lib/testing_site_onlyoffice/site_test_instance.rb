28dc73d89ada0ae0d506a16f4e88b0ec78023afe
Olga Nesterova <olganesterova034@gmail.com>
2021-04-13 23:51:43 -0700

    Delete portal secret data

--- lib/testing_site_onlyoffice/site_test_instance.rb
+++ lib/testing_site_onlyoffice/site_test_instance.rb
@@ -25,13 +25,10 @@ include OnlyofficeWebdriverWrapper
 module TestingSiteOnlyoffice
   class SiteTestInstance
     attr_accessor :webdriver, :doc_instance
-    # # @return [Hash] list of secret data
-    # attr_reader :secret_data
     alias driver webdriver
     alias selenium webdriver
 
     def initialize(config)
-      # @secret_data = SecretData.new.decrypt
       @webdriver = WebDriver.new(config.browser, record_video: false)
       url = config.server.end_with?('teamlab.info') ? "#{config.server}?Site_Testing=4testing" : config.server
       @webdriver.open(url)
