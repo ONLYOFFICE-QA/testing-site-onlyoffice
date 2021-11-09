524888cd99fdd51cdfd8c071c3d733f9e4dd25d9
Alexandr Mochalov <Alexander.Mochalov@onlyoffice.com>
2021-11-09 15:22:05 +0300

    Add new test for sign up onlyoffice doc

--- lib/testing_site_onlyoffice/get_onlyoffice/onlyoffice_docs/site_doсs_registration_page/site_docs_registration_data.rb
+++ lib/testing_site_onlyoffice/get_onlyoffice/onlyoffice_docs/site_doсs_registration_page/site_docs_registration_data.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module TestingSiteOnlyoffice
-  # Default admin data for creating portal
+  # Default online document data for sign up for cloud
   class DocsRegistrationData
-    attr_accessor :first_name, :last_name, :doc_email, :doc_phone
+    attr_reader :first_name, :last_name, :doc_email, :doc_phone
 
     def initialize
       @first_name = Faker::Name.first_name
-      @last_name = Faker::Name.first_name
+      @last_name = Faker::Name.last_name
       @doc_email = SiteData::EMAIL_ADMIN
       @doc_phone = Faker::PhoneNumber.cell_phone_in_e164
     end
