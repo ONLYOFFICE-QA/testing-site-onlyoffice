4f06c8e907bf577c79873137d5257843b177f40c
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-11-27 21:18:27 +0200

    Fix problem with getting mail (#543)
    In all those tests there is 2 times getting mail
    And first time will mark all mail in inbox as read
    And second time will only search for unread mail
    
    Fix #538

--- spec/functional/pricing/site_buy_notification_spec.rb
+++ spec/functional/pricing/site_buy_notification_spec.rb
@@ -22,7 +22,7 @@ describe 'Buy Product Notification' do
     avangate = pricing_page.go_to_avangate_from_pricing_page(pricing_page.buy_now_enterprise_plus_element, test_purchase: true)
     avangate.submit_avangate_order_for_notification(email: @mail.username)
     expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PAYMENT_RECEIVED }, 300, true)).to be_truthy
-    expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PURCHASE_WORKSPACE }, 300, true)).to be_truthy
+    expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PURCHASE_WORKSPACE, include_read: true }, 300, true)).to be_truthy
   end
 
   it '[Site][PricingDocsEnterprise] Buy Docs Enterprise Edition and check notify /docs-enterprise-prices.aspx' do
@@ -31,18 +31,17 @@ describe 'Buy Product Notification' do
     avangate = pricing_page.go_to_avangate_from_pricing_page(pricing_page.buy_now_single_server_element, test_purchase: true)
     avangate.submit_avangate_order_for_notification(email: @mail.username)
     expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PAYMENT_RECEIVED }, 300, true)).to be_truthy
-    expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PURCHASE_DOCS_ENTERPRISE }, 300,
+    expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PURCHASE_DOCS_ENTERPRISE, include_read: true }, 300,
                                         true)).to be_truthy
   end
 
   it '[Site][PricingDocsEnterprise] Buy Docs Enterprise Edition for home and check notify /docs-enterprise-prices.aspx' do
-    skip('The second letter does not arrive in the mail')
     pricing_page = @site_home_page.click_link_on_toolbar(:pricing_enterprise)
     pricing_page.choose_home_tariff
     avangate = pricing_page.go_to_avangate_from_pricing_page(pricing_page.buy_now_home_server_element, test_purchase: true)
     avangate.submit_avangate_order_for_notification(email: @mail.username)
     expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PAYMENT_RECEIVED }, 300, true)).to be_truthy
-    expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PURCHASE_DOCS_ENTERPRISE }, 300,
+    expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PURCHASE_DOCS_ENTERPRISE, include_read: true }, 300,
                                         true)).to be_truthy
   end
 
@@ -51,7 +50,7 @@ describe 'Buy Product Notification' do
     avangate = pricing_page.go_to_avangate_from_pricing_page(pricing_page.buy_now_single_server_element, test_purchase: true)
     avangate.submit_avangate_order_for_notification(email: @mail.username)
     expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PAYMENT_RECEIVED }, 300, true)).to be_truthy
-    expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PURCHASE_DOCS_DEVELOPER }, 300,
+    expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PURCHASE_DOCS_DEVELOPER, include_read: true }, 300,
                                         true)).to be_truthy
   end
 end
