a3e5f98613a792210fe9e3b75724ee1825494036
Alexandr Mochalov <67409742+amochalov85@users.noreply.github.com>
2021-12-01 16:34:55 +0300

    Add new tests for download apps (#553)

--- spec/functional/about/site_about_blogs_spec.rb
+++ spec/functional/about/site_about_blogs_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 test_manager = TestingSiteOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
 
-describe 'Training courses' do
+describe 'About blogs' do
   before do
     site_home_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
     @about_blogs_page = site_home_page.click_link_on_toolbar(:about_blog)
@@ -15,7 +15,21 @@ describe 'Training courses' do
     @test.webdriver.quit
   end
 
-  it "[Training courses] Number of modules didn't change" do
+  it '[About blogs] Names of download buttons' do
     expect(@about_blogs_page.desktop_and_mobile_app).to eq(TestingSiteOnlyoffice::SiteData.blogs_download_app)
+  end
+
+  it '[About blogs] Go to app GooglePlay' do
+    expect(@about_blogs_page.check_google_play_link).to be true
+  end
+
+  it '[About blogs] Go to app App Store' do
+    expect(@about_blogs_page.check_app_store_link).to be true
+  end
+
+  TestingSiteOnlyoffice::SiteData.download_desktop_apps.each do |apps|
+    it "[About blogs] Go to desktop #{apps}" do
+      expect(@about_blogs_page.go_to_desktop_apps(apps)).to be_a TestingSiteOnlyoffice::SiteDesktopApps
+    end
   end
 end
