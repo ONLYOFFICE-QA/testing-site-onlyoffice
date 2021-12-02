12a27ac3f387561ee08eb42aa406a8eb29ef04ab
Alexandr Mochalov <67409742+amochalov85@users.noreply.github.com>
2021-12-02 12:38:34 +0300

    Add new tests for contributions (#556)

---
+++ spec/functional/about/site_about_contribute_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+test_manager = TestingSiteOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
+
+describe 'About contribute' do
+  before do
+    site_home_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
+    @about_contribute_page = site_home_page.click_link_on_toolbar(:about_contribute)
+  end
+
+  after do |example|
+    test_manager.add_result(example, @test)
+    @test.webdriver.quit
+  end
+
+  it '[About contribute] Go to documentation Github' do
+    @about_contribute_page.click_read_api_documentation(3)
+    expect(@about_contribute_page.check_title_documentation_github).to be true
+  end
+
+  it '[About contribute] Go to documentation community_server' do
+    @about_contribute_page.click_read_api_documentation(2)
+    expect(@about_contribute_page.check_title_documentation_community_server).to be true
+  end
+
+  it '[About contribute] Go to documentation plagins' do
+    @about_contribute_page.click_read_api_documentation(1)
+    expect(@about_contribute_page.check_title_documentation_plagins).to be true
+  end
+
+  it '[About contribute] Go to documentation connectors' do
+    expect(@about_contribute_page.check_title_documentation_connectors).to be true
+  end
+end
