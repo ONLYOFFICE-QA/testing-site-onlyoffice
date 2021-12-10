d76417c8bbd4201a2d7ce20660c419d703f272e6
Alexandr Mochalov <Alexander.Mochalov@onlyoffice.com>
2021-12-10 15:00:57 +0300

    Separate spec for connectors

--- spec/functional/products/site_products_partner_connectors_spec.rb
+++ spec/functional/products/site_products_partner_connectors_spec.rb
@@ -43,25 +43,4 @@ describe 'Site products connectors' do
       end
     end
   end
-
-  TestingSiteOnlyoffice::SiteDownloadData.connectors_list.each do |connector|
-    describe connector.to_s do
-      let(:current_connector) { @products_connectors_page.onlyoffice_connector_block(connector) }
-
-      it "[Site][Products][OnlyofficeConnectors][#{connector}] 'More info' link works /all-connectors.aspx" do
-        connector_info_page = @products_connectors_page.click_more_info_link(current_connector.more_info_xpath, connector.to_s)
-        expect(connector_info_page).to be_logo_element_present
-      end
-
-      it "[Site][Products][OnlyofficeConnectors][#{connector}] Developer website link works /all-connectors.aspx" do
-        @products_connectors_page.click_constructor_link(current_connector.developer_website_xpath)
-        expect(@products_connectors_page).to be_onlyoffice_opened
-      end
-
-      it "[Site][Products][OnlyofficeConnectors][#{connector}] 'Get it now' button works /all-connectors.aspx" do
-        download_connectors_page = @products_connectors_page.click_get_it_now_link(current_connector.get_it_now_xpath)
-        expect(download_connectors_page).to be_a TestingSiteOnlyoffice::SiteOtherProductsConnectors
-      end
-    end
-  end
 end
