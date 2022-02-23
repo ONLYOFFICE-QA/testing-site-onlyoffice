637a371d28dcc108263ef0bf4d66bf0bdcb043aa
Alexandr Mochalov <alexander.mochalov@onlyoffice.com>
2022-02-23 17:25:43 +0300

    Add errors for find massage

--- lib/testing_site_onlyoffice/site_notification_helper.rb
+++ lib/testing_site_onlyoffice/site_notification_helper.rb
@@ -28,7 +28,7 @@ module TestingSiteOnlyoffice
                                                 params[:times] = params.fetch(:times, 300),
                                                 params[:move_out] = params.fetch(:move_out, true))
 
-      raise "Didn't find any messages for #{expected_string}" if mail.nil?
+      raise "Didn't find any messages for #{expected_string}" unless mail
 
       mail[:html_body].split('href="').grep(/confirm.aspx/)[0].split('"')[0]
     end
