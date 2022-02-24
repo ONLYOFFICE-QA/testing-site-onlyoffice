071b41bd16523d405e4d1eeceff9c14eb3fd6922
Alexandr Mochalov <67409742+amochalov85@users.noreply.github.com>
2022-02-24 12:04:04 +0300

    Add errors for find massage (#685)

--- lib/testing_site_onlyoffice/site_notification_helper.rb
+++ lib/testing_site_onlyoffice/site_notification_helper.rb
@@ -27,6 +27,9 @@ module TestingSiteOnlyoffice
                                                   search: params[:search] },
                                                 params[:times] = params.fetch(:times, 300),
                                                 params[:move_out] = params.fetch(:move_out, true))
+
+      raise "Didn't find any messages with title `#{expected_string}`" unless mail
+
       mail[:html_body].split('href="').grep(/confirm.aspx/)[0].split('"')[0]
     end
 
