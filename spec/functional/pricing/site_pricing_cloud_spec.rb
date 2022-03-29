134002c99bf65cb261d95647dee5274a15e5ef20
Alexandr Mochalov <67409742+amochalov85@users.noreply.github.com>
2022-03-29 11:54:27 +0300

    New tariffs for registration cloud (#725)

--- spec/functional/pricing/site_pricing_cloud_spec.rb
+++ spec/functional/pricing/site_pricing_cloud_spec.rb
@@ -31,8 +31,8 @@ describe 'Pricing Cloud Service' do
         expect(sign_up_page).to be_a TestingSiteOnlyoffice::SiteSignUp
       end
 
-      it "[Site][Pricing][Cloud] VIP check `Contact us` button for #{period}" do
-        expect(@pricing_cloud_page).to be_vip_contact_us_works
+      it "[Site][Pricing][Cloud] PERSONAL check `Contact us` button for #{period}" do
+        expect(@pricing_cloud_page).to be_personal_contact_us_works
       end
     end
   end
@@ -71,7 +71,7 @@ describe 'Pricing Cloud Service' do
   it '[Site][Pricing][Cloud] Fill `Contact us` form for VIP plan and check notify' do
     pending('https://bugzilla.onlyoffice.com/show_bug.cgi?id=43150') if config.server.include?('.com')
     company_name = "NCT Test #{Faker::Company.name}"
-    vip_request_page = @pricing_cloud_page.vip_contact_us
+    vip_request_page = @pricing_cloud_page.vip_submit_request
     vip_request_page.send_vip_cloud_request(company_name: company_name)
     subject_message = "#{company_name}#{TestingSiteOnlyoffice::SiteNotificationHelper.site_translate_from_resource(config.language, 'contact_vip', 'WebStudio')}"
     expect(partner_email.check_email_by_subject({ subject: subject_message }, 300, true)).to be_truthy
