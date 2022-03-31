b22077f7973f0770a31505feed81a367aa839b7e
Alexandr Mochalov <alexander.mochalov@onlyoffice.com>
2022-03-31 14:04:47 +0300

    Refactoring code for naming

--- spec/functional/about/site_about_awards_spec.rb
+++ spec/functional/about/site_about_awards_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 test_manager = TestingSiteOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
 
-describe 'Customer stories' do
+describe 'About awards' do
   before do
     site_home_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
     @awards = site_home_page.click_link_on_toolbar(:about_awards)
