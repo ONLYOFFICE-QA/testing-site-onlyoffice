832273055b976a9b399c42d4555cd03e895913e8
Alexandr Mochalov <67409742+amochalov85@users.noreply.github.com>
2022-04-04 16:37:37 +0300

    Fix get url affiliatest (#738)

--- lib/testing_site_onlyoffice/partners/site_partners_affiliates.rb
+++ lib/testing_site_onlyoffice/partners/site_partners_affiliates.rb
@@ -40,8 +40,10 @@ module TestingSiteOnlyoffice
       attribute = @instance.webdriver.get_attribute(sign_in_an_affiliates_element, 'href')
       sign_in_an_affiliates_element.click
       @instance.webdriver.switch_to_popup
-      url = @instance.webdriver.get_url
-      attribute == url
+      url = URI(@instance.webdriver.get_url)
+      url.fragment = url.query = nil
+      parse_url = current_url.to_s
+      attribute.include?(parse_url)
     end
   end
 end
