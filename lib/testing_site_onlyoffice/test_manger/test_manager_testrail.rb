8a4e34f01b0610339d59579ad81805caf8698556
Pavel Lobashov <shockwavenn@gmail.com>
2022-04-16 01:29:16 +0300

    Remove deprecated `search_plan_by_substring` configuration

--- lib/testing_site_onlyoffice/test_manger/test_manager_testrail.rb
+++ lib/testing_site_onlyoffice/test_manger/test_manager_testrail.rb
@@ -10,7 +10,6 @@ module TestingSiteOnlyoffice
 
       @testrail = TestrailHelper.new(params[:product_name], params[:suite_name], params[:plan_name_testrail]) do |testrail_conf|
         testrail_conf.add_all_suites = true
-        testrail_conf.search_plan_by_substring = false
       end
     end
   end
