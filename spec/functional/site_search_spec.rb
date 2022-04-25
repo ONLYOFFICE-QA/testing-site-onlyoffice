c10f8841869b7d4ec8b09297b177b0bade354190
Alexandr Mochalov <alexander.mochalov@onlyoffice.com>
2022-04-25 13:14:04 +0300

    Change string for search site

--- spec/functional/site_search_spec.rb
+++ spec/functional/site_search_spec.rb
@@ -12,7 +12,7 @@ describe 'Site search' do
   end
 
   describe 'non-existing word' do
-    let(:result_page) { @site_home_page.search('Fakeword') }
+    let(:result_page) { @site_home_page.search('Asdfgh') }
 
     it 'Non-existing word return No results matching your query page and zero found results' do
       expect(result_page.no_result_found_element).to be_present
