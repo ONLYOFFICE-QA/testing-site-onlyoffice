f17cdfb9ae80271e5d5ba3e1de667691e6d160e8
Kobiljon Komilov <qobiljon3108@gmail.com>
2022-09-06 14:43:21 +0500

    Add spec for file upload

--- lib/testing_site_onlyoffice/convert_page.rb
+++ lib/testing_site_onlyoffice/convert_page.rb
@@ -3,13 +3,19 @@
 require_relative 'site_helper'
 
 module TestingSiteOnlyoffice
+
   # Handles operations with convert page
   # /convert.aspx
   # https://user-images.githubusercontent.com/38238032/188140519-e3b46efa-34a9-4aad-9802-3eb6ff32c68a.png
   class ConvertPage
+    include PageObject
+
+    file_field(:uploader, xpath: '//*[@id="fileInput"]')
+
     def initialize(instance)
       @instance = instance
-      @file_input_xpath = '//*[@id="fileInput"]'
+      @file_input_field_xpath = '//*[@id="fileInput"]'
+      @convert_error_message_xpath = "//p[contains(text(), 'The file format is not supported.')]"
       wait_to_load
     end
 
@@ -20,7 +26,11 @@ module TestingSiteOnlyoffice
 
     # Check if Input field is present on the page
     def file_input_present?
-      @instance.webdriver.element_present?(@file_input_xpath)
+      @instance.webdriver.element_present?(@file_input_field_xpath)
+    end
+
+    def upload_file(file_path)
+      self.uploader = file_path
     end
   end
 end
--- spec/functional/open_convert_page_spec.rb
+++ spec/functional/open_convert_page_spec.rb
@@ -3,10 +3,16 @@
 require 'spec_helper'
 
 test_manager = TestingSiteOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
+main_path = "#{Dir.home}/RubymineProjects/testing-site-onlyoffice/lib/testing_site_onlyoffice/data/"
+doc_file_path = "#{main_path}testing_upload_document.docx"
+spreadsheet_file_path = "#{main_path}testing_upload_spreadsheet.xls"
+presentation_file_path = "#{main_path}testing_upload_presentation.pptx"
+log_file_path = "#{main_path}testing_upload_log.log"
 
 describe 'Convert page' do
   before do
     @site_main_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
+    @convert_page = @site_main_page.open_convert_page
   end
 
   after do |example|
@@ -15,7 +21,10 @@ describe 'Convert page' do
   end
 
   it 'Open convert page' do
-    convert_page = @site_main_page.open_convert_page
-    expect(convert_page).to be_file_input_present
+    expect(@convert_page).to be_file_input_present
+  end
+
+  it 'Upload .doc file' do
+    @convert_page.upload_file(doc_file_path)
   end
 end
