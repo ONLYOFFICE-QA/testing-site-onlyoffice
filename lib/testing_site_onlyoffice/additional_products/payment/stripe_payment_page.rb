5695221408bd58db7208881cb530212eba54995d
Qobiljonn <qobiljon3108@gmail.com>
2022-11-22 16:30:03 +0500

    Fix spec for document registration page and buy notification

--- lib/testing_site_onlyoffice/additional_products/payment/stripe_payment_page.rb
+++ lib/testing_site_onlyoffice/additional_products/payment/stripe_payment_page.rb
@@ -8,7 +8,8 @@ class StripePaymentPage
   def initialize(instance)
     @instance = instance
     @xpath_submit_button = '//div[contains(@class,"SubmitButton-IconContainer")]'
-    @xpath_zip_input = '//*[@id="billingPostalCode"]'
+    @xpath_zip_billing = '//*[@id="billingPostalCode"]'
+    @xpath_zip_shipping = '//*[@id="shippingPostalCode"]'
     @xpath_billing_address = '//div[contains(@class, "BillingAddressForm-shippingAsBillingCheckbox ")]'
     wait_to_load
   end
@@ -23,6 +24,11 @@ class StripePaymentPage
   # Submit order for notification
   def submit_order_for_notification(params = {})
     @instance.webdriver.type_to_locator('//*[@id="email"]', params[:email], true, true)
+    @instance.webdriver.type_to_locator('//*[@id = "shippingName"]', 'Teamlab Ruby', true, true)
+    @instance.webdriver.type_to_locator('//*[@id = "shippingAddressLine1"]', '5302 Dublin Ave', true, true)
+    enter_second_address
+    @instance.webdriver.type_to_locator('//*[@id = "shippingLocality"]', 'New York', true, true)
+    enter_zip_billing
     @instance.webdriver.type_to_locator('//*[@id="cardNumber"]', '4242424242424242', true, true)
     @instance.webdriver.type_to_locator('//*[@id="cardExpiry"]', '1250', true, true)
     @instance.webdriver.type_to_locator('//*[@id="cardCvc"]', '111', true, true)
@@ -30,7 +36,7 @@ class StripePaymentPage
     @instance.webdriver.click_on_locator(@xpath_billing_address)
     @instance.webdriver.wait_until { billing_address_block_visible? }
     @instance.webdriver.type_to_locator('//*[@id="billingName"]', 'Teamlab Ruby', true, true)
-    enter_zip
+
 
     @instance.webdriver.click_on_locator(@xpath_submit_button)
     wait_for_order_finish
@@ -53,14 +59,33 @@ class StripePaymentPage
 
   # Enter zip number if Stripe asked for it
   # @return [nil]
-  def enter_zip
-    return unless @instance.webdriver.element_visible?(@xpath_zip_input)
+  def enter_zip_billing
+    return unless @instance.webdriver.element_visible?(@xpath_zip_billing)
 
-    @instance.webdriver.type_to_locator(@xpath_zip_input, '12345', true, true)
+    @instance.webdriver.type_to_locator(@xpath_zip_billing, '12345', true, true)
+  end
+
+  def enter_zip_shipping
+    return unless @instance.webdriver.element_visible?(@xpath_zip_shipping)
+
+    @instance.webdriver.type_to_locator(@xpath_zip_shipping, '12345', true, true)
   end
 
   def billing_address_block_visible?
     xpath_billing_address_block = '//div[contains(@class, "BillingAddressForm-addressInput")]'
     @instance.webdriver.get_attribute(xpath_billing_address_block, 'aria-hidden')
+  end
+
+  def enter_second_address
+    xpath_second_address_line = '//*[@id = "shippingAddressLine2"]'
+    return unless @instance.webdriver.element_present?(xpath_second_address_line)
+
+    @instance.webdriver.type_to_locator(xpath_second_address_line, 'Springfield, Virginia(VA)', true, true)
+  end
+
+  def select_state
+    xpath_state = '//*[@id = "shippingAdministrativeArea"]'
+    values = @instance.webdriver.get_all_combo_box_values(xpath_state)
+    @instance.select_combo_box(xpath_state, values.first)
   end
 end
