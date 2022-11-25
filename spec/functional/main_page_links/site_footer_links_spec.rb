660cec178c15c16647bec15cb83e0f62e8fb774a
Qobiljonn <qobiljon3108@gmail.com>
2022-11-25 18:32:53 +0500

    Fix small typo in spec names and add pending in footer_links_spec

--- spec/functional/main_page_links/site_footer_links_spec.rb
+++ spec/functional/main_page_links/site_footer_links_spec.rb
@@ -17,6 +17,7 @@ describe 'Help center footer links' do
     titles.each do |title|
       it "[Site] `#{title}` link of `#{section_title}` footer section works" do
         pending('Link `https://www.linkedin.com/company/ascensio-system-sia/` answered with 999') if title == 'Follow us on LinkedIn'
+        pending('Instagram is blocking access from autotests with 405 error') if title == 'Follow us on Instagram'
         expect(@site_home_page).to be_site_footer_link_alive(section_title, title)
       end
     end
