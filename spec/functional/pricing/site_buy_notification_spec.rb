520a5ccbfb408aac6f7fb99bd1e183fcf506586a
Qobiljonn <qobiljon3108@gmail.com>
2022-11-30 17:05:27 +0500

    Fix for workaround was implemented so reverting changes

--- spec/functional/pricing/site_buy_notification_spec.rb
+++ spec/functional/pricing/site_buy_notification_spec.rb
@@ -40,7 +40,7 @@ describe 'Buy Product Notification' do
     skip('Stripe payments cannot be checked on production') if config.server.include?('.com')
     pricing_page = @site_home_page.click_link_on_toolbar(:pricing_enterprise)
     pricing_page.choose_home_tariff
-    avangate = pricing_page.go_to_avangate_from_pricing_page(pricing_page.buy_now_single_server_element, test_purchase: true)
+    avangate = pricing_page.go_to_avangate_from_pricing_page(pricing_page.buy_now_home_server_element, test_purchase: true)
     avangate.submit_order_for_notification(email: @mail.username)
     expect(@mail.check_email_by_subject({ subject: TestingSiteOnlyoffice::SiteNotificationData::PURCHASE_DOCS_ENTERPRISE, include_read: true }, 300,
                                         true)).to be_truthy
