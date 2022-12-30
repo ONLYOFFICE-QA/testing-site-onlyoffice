26efb1f920790652e83822fcbb0dba4443b71ac6
Kobiljon Komilov <38238032+Qobiljonn@users.noreply.github.com>
2022-12-30 18:38:50 +0500

    Change convert page spec and add tests to it (#1148)

--- lib/testing_site_onlyoffice/features/site_features_pdf_reader_converter.rb
+++ lib/testing_site_onlyoffice/features/site_features_pdf_reader_converter.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../convert_page'
+
 module TestingSiteOnlyoffice
   # /pdf-reader.aspx
   # https://user-images.githubusercontent.com/38238032/196688860-a0e3bf90-d959-4e1c-8b26-e2d7de607e3a.png
@@ -7,7 +9,7 @@ module TestingSiteOnlyoffice
     include PageObject
     include SiteToolbar
 
-    link(:convert_link, xpath: '//a[@href = "/convert.aspx?from=pdfreader"]')
+    link(:convert_link, xpath: '//a[@href = "/online-document-converter.aspx?from=pdfreader"]')
 
     def initialize(instance)
       super(instance.webdriver.driver)
@@ -19,6 +21,11 @@ module TestingSiteOnlyoffice
       @instance.webdriver.wait_until do
         @instance.webdriver.element_present?(convert_link_element)
       end
+    end
+
+    def open_convert_page
+      convert_link_element.click
+      ConvertPage.new(@instance)
     end
   end
 end
