f36fc6d058311644c0cc444dd60556fd34a52dec
Kobiljon Komilov <38238032+Qobiljonn@users.noreply.github.com>
2023-03-23 14:08:02 +0500

    Update site_onlyoffice_docs_enterprise_spec (#1253)

--- lib/testing_site_onlyoffice/modules/site_toolbar.rb
+++ lib/testing_site_onlyoffice/modules/site_toolbar.rb
@@ -52,7 +52,7 @@ module TestingSiteOnlyoffice
     link(:site_get_onlyoffice_connectors, xpath: '//a[@id="navitem_download_connectors"]')
     link(:site_get_onlyoffice_desktop_mobile, xpath: '//a[@id="navitem_download_desktop_mob"]')
     link(:site_get_onlyoffice_docs_developer, xpath: '//a[@id="navitem_download_docs_de"]')
-    link(:site_get_onlyoffice_docs_enterprise, xpath: '//a[@id="navitem_docs_onpremises"]')
+    link(:site_get_onlyoffice_docs_enterprise, xpath: '//a[@id="navitem_download_docs_ee"]')
     link(:site_get_onlyoffice_docs_community, xpath: '//a[@id="navitem_download_docs_ce"]')
     link(:site_get_onlyoffice_document_builder, xpath: '//a[@id="navitem_download_docs_builder"]')
     link(:site_get_onlyoffice_web_hosting, xpath: '//a[@id="navitem_download_hosting"]')
