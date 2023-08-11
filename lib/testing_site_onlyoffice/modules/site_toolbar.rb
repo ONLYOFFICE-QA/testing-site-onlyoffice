05c9ad0f356a483a98dfd2b5c26227a1f988e3f6
Qobiljonn <qobiljon3108@gmail.com>
2023-08-11 20:52:35 +0500

    Revert Pricing Cloud changes

--- lib/testing_site_onlyoffice/modules/site_toolbar.rb
+++ lib/testing_site_onlyoffice/modules/site_toolbar.rb
@@ -66,6 +66,7 @@ module TestingSiteOnlyoffice
     # top toolbar - Pricing
     link(:site_pricing, xpath: '//a[@id="navitem_prices"]')
     link(:site_pricing_docs_enterprise, xpath: '//a[@id="navitem_prices_docs_enterprice"]')
+    link(:site_pricing_workspace, xpath: '//a[@id="navitem_prices_workspace"]')
     link(:site_pricing_docspace, xpath: '//a[@id="navitem_prices_docspace"]')
     link(:site_pricing_docs_developer, xpath: '//a[@id="navitem_prices_docs_dev"]')
     link(:site_pricing_buy_from_reseller, xpath: '//a[@id="navitem_prices_reseller"]')
@@ -256,9 +257,12 @@ module TestingSiteOnlyoffice
           element: site_pricing_docs_enterprise_element,
           class: SitePriceDocsEnterprise
         },
+        pricing_workspace: {
+          element: site_pricing_workspace_element,
+          class: SitePricingCloud
+        },
         pricing_docspace: {
           element: site_pricing_docspace_element,
-          class: SitePricingCloud
         },
         pricing_developer: {
           element: site_pricing_docs_developer_element,
