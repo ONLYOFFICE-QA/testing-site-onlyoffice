35468837fe4f5133e3fb175c2546e3fe60bcf9c0
Kobiljon Komilov <38238032+Qobiljonn@users.noreply.github.com>
2023-09-13 19:10:36 +0500

    Update site footer links (#1468)
    * Update site footer links
    
    * Update footer links
    
    * Update footer links info

--- spec/functional/main_page_links/site_footer_links_spec.rb
+++ spec/functional/main_page_links/site_footer_links_spec.rb
@@ -18,6 +18,9 @@ describe 'Help center footer links' do
       it "[Site] `#{title}` link of `#{section_title}` footer section works" do
         pending('Link `https://www.linkedin.com/company/ascensio-system-sia/` answered with 999') if title == 'Follow us on LinkedIn'
         pending('Instagram is blocking access from autotests with 405 error') if title == 'Follow us on Instagram'
+        pending('Medium is blocking access from autotests with 405 error') if title == 'Follow us on Medium'
+        pending('TikTok is blocking access from autotests with 405 error') if title == 'Follow us on TikTok'
+        pending('Twitter is blocking access from autotests with 405 error') if title == 'Follow us on Twitter'
         expect(@site_home_page).to be_site_footer_link_alive(section_title, title)
       end
     end
