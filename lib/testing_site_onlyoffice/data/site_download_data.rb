4d451cd9c26db33c460c3bb35093cc99d031f61c
Kobiljon Komilov <38238032+Qobiljonn@users.noreply.github.com>
2023-09-27 15:24:37 +0500

    Remove debian 7 support from hourly (#1487)

--- lib/testing_site_onlyoffice/data/site_download_data.rb
+++ lib/testing_site_onlyoffice/data/site_download_data.rb
@@ -6,7 +6,7 @@ module TestingSiteOnlyoffice
     # Desktop
     def self.desktop_download_list_type
       {
-        download_file: %i[macos_x86 debian_8 debian_7 centos appimage],
+        download_file: %i[macos_x86 debian_8 centos appimage],
         three_download_windows_files: %i[windows_10_8_7_x64 windows_10_8_7_x86 windows_xp],
         two_download_mac_files: %i[macos_10],
         download_from_store: %i[snap flatpak]
