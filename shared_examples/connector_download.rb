1bb45c22e1ffcd3792c3c76c4ac8f9c99fda303d
Kobiljon Komilov <38238032+Qobiljonn@users.noreply.github.com>
2024-02-13 17:56:59 +0500

    Update connectors shared example (#1685)

--- shared_examples/connector_download.rb
+++ shared_examples/connector_download.rb
@@ -10,24 +10,7 @@ shared_examples_for 'connector_download' do |connectors_list|
       it "[Site][Connectors][#{connector}] 'Get on Github' button works /download-connectors.aspx" do
         connectors_page.click_constructor_link(@current_installation.get_on_github_xpath)
         github_title = TestingSiteOnlyoffice::SiteDownloadData.connectors_info[connector.to_s]['github']
-        expect(connectors_page.check_opened_page_title).to eq(github_title)
-      end
-
-      it "[Site][Connectors][#{connector}] 'Read installation' button works /download-connectors.aspx" do
-        pending 'Connector instructions missing' if connector == 'odoo'
-        connectors_page.click_constructor_link(@current_installation.instruction_xpath)
-        instruction_title = TestingSiteOnlyoffice::SiteDownloadData.connectors_info[connector.to_s]['instruction']
-        expect(connectors_page.check_opened_page_title).to eq(instruction_title)
-      end
-
-      it "[Site][Connectors][#{connector}]release info on site matches github data /download-connectors.aspx" do
-        site_data = TestingSiteOnlyoffice::SiteConnectorReleaseData.new(version: connectors_page.get_installer_release_date_or_version(@current_installation.version_xpath),
-                                                                        date: connectors_page.get_installer_release_date_or_version(@current_installation.release_date_xpath))
-        github_data = TestingSiteOnlyoffice::SiteConnectorReleaseData.new(version: connectors_page.github_version(connector),
-                                                                          date: connectors_page.github_date(connector))
-        error = connectors_page.error_version_message(connector:, github_date: github_data.date,
-                                                      github_version: github_data.version, site_date: site_data.date, site_version: site_data.version)
-        expect(github_data == site_data).to be_truthy, error
+        expect(connectors_page.check_opened_page_title(switch_tab: false)).to eq(github_title)
       end
     end
   end
