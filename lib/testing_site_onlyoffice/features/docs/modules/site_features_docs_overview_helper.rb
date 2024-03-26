9ff34924b8e43bef2952dacef1ae1ca7bbe0ba1c
Julia B <bubnovaum@gmail.com>
2024-03-26 18:20:36 +0400

    minor fix after review

--- lib/testing_site_onlyoffice/features/docs/modules/site_features_docs_overview_helper.rb
+++ lib/testing_site_onlyoffice/features/docs/modules/site_features_docs_overview_helper.rb
@@ -2,8 +2,46 @@
 
 module TestingSiteOnlyoffice
   # Helper for storing additional methods of the class SiteFeaturesDocsOverview
-  # Contains methods for working with the "choose where to work" block.
   module SiteFeaturesDocsOverviewHelper
+
+    def click_get_it_now_top
+      @instance.webdriver.click_on_locator(get_it_now_top_element)
+      SiteGetOnlyofficeDocsEnterprise.new(@instance)
+    end
+
+    def click_see_it_action
+      @instance.webdriver.click_on_locator(see_it_in_action_element)
+      SiteFeaturesSeeItInAction.new(@instance)
+    end
+
+    def click_find_the_plugins
+      xpath = '//a[contains(@href, "app-directory") and contains(@class, "ca_link")]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteFeaturesMarketplace.new(@instance)
+    end
+
+    def click_security_learn_more
+      xpath = '//a[contains(@href, "security.aspx") and contains(@class, "lm-link")]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteFeaturesSecurity.new(@instance)
+    end
+
+    def click_download_pc
+      xpath = '//*[@id="office_suite_download_desktop_download_now"]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteGetOnlyofficeDesktopApps.new(@instance)
+    end
+
+    def click_install_mobile
+      xpath = '//*[@id="office_suite_download_desktop_mobile_install_now"]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteMobileApps.new(@instance)
+    end
+
     def click_docspace_start_free
       xpath = '//*[@id="office_suite_docspace_registration_start_with_your_free_account"]'
       @instance.webdriver.move_to_element_by_locator(xpath)
