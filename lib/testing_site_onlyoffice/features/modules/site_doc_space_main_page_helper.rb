387279d7aba814c0630f8f11f5cefb2c89ebbe90
Julia Bubnova <99170537+JuliaBuvx@users.noreply.github.com>
2024-03-28 16:22:53 +0400

    add new tests for Products - DocSpace (#1741)

---
+++ lib/testing_site_onlyoffice/features/modules/site_doc_space_main_page_helper.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+module TestingSiteOnlyoffice
+  # Helper for storing methods of the class SiteFeaturesDocsOverview
+  module SiteDocSpaceMainPageHelper
+    def click_registration_button
+      docspace_registration_button_element.click
+      SiteDocSpaceSignUp.new(@instance)
+    end
+
+    def click_zoom_collaboration_learn_more
+      xpath = '//a[@class = "ds-ta-link" and @href = "/office-for-zoom.aspx"]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteConnectorsZoom.new(@instance)
+    end
+
+    def click_security_learn_more
+      xpath = '//a[@class = "lm_link" and @href = "/security.aspx"]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteFeaturesSecurity.new(@instance)
+    end
+
+    def click_free_cloud_startups
+      startups_free_cloud_element.click
+      SiteDocSpaceSignUp.new(@instance)
+    end
+
+    def click_business_check_prices
+      xpath = '//div[@class="choice_item"]//a[contains(@href, "docspace-prices.aspx")]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SitePricingDocSpacePrices.new(@instance)
+    end
+
+    def click_enterprise_get_it_now
+      enterprise_get_it_now_element.click
+      SiteDocSpaceDownloadEnterprise.new(@instance)
+    end
+
+    def click_docspace_start_free_cloud
+      xpath = '//a[@id = "docspace_docspace_registration_start_with_your_free_account"]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteDocSpaceSignUp.new(@instance)
+    end
+
+    def click_deploy_on_your_own_server
+      xpath = '//a[@id = "docspace_docspace_deploy_your_own_server"]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteDocSpaceDownloadEnterprise.new(@instance)
+    end
+
+    def click_connectors_get_it_now
+      xpath = '//a[@id = "docspace_docspace_get_it_now"]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteConnectorsOnlyoffice.new(@instance)
+    end
+
+    def click_download_pc_now
+      xpath = '//a[@id = "docspace_download_desktop_download_now"]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteGetOnlyofficeDesktopApps.new(@instance)
+    end
+
+    def click_mobile_install_now
+      xpath = '//a[@id = "docspace_download_desktop_mobile_install_now"]'
+      @instance.webdriver.move_to_element_by_locator(xpath)
+      @instance.webdriver.click_on_locator(xpath)
+      SiteMobileApps.new(@instance)
+    end
+  end
+end
---
+++ spec/functional/features/site_products_doc_space_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+test_manager = TestingSiteOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
+
+describe 'Site Products DocSpace' do
+  before do
+    site_home_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
+    @docspace_page = site_home_page.click_link_on_toolbar(:features_docspace)
+  end
+
+  after do |example|
+    test_manager.add_result(example, @test)
+    @test&.webdriver&.quit
+  end
+
+  TestingSiteOnlyoffice::SiteDocSpaceMainPage::FEATURES_LINKS.each do |feature_key, feature_info|
+    it_behaves_like 'checking_editors_links', feature_key, feature_info
+    let(:page) { @docspace_page }
+  end
+
+  it '[Site Products] [DocsSpace] Create account button works' do
+    expect(@docspace_page.click_registration_button).to be_a TestingSiteOnlyoffice::SiteDocSpaceSignUp
+  end
+
+  it '[Site Products] [DocsSpace] "Zoom collaboration / learn more" button works' do
+    expect(@docspace_page.click_zoom_collaboration_learn_more).to be_a TestingSiteOnlyoffice::SiteConnectorsZoom
+  end
+
+  it '[Site Products] [DocsSpace] "Security / learn more" button works' do
+    expect(@docspace_page.click_security_learn_more).to be_a TestingSiteOnlyoffice::SiteFeaturesSecurity
+  end
+
+  it '[Site Products] [DocsSpace] "Free cloud for startups" button works' do
+    expect(@docspace_page.click_free_cloud_startups).to be_a TestingSiteOnlyoffice::SiteDocSpaceSignUp
+  end
+
+  it '[Site Products] [DocsSpace] For business "check prices" button works' do
+    expect(@docspace_page.click_business_check_prices).to be_a TestingSiteOnlyoffice::SitePricingDocSpacePrices
+  end
+
+  it '[Site Products] [DocsSpace] "Enterprise - get it now" button works' do
+    expect(@docspace_page.click_enterprise_get_it_now).to be_a TestingSiteOnlyoffice::SiteDocSpaceDownloadEnterprise
+  end
+
+  it '[Site Products] [DocsSpace] "Start free cloud account" button works' do
+    expect(@docspace_page.click_docspace_start_free_cloud).to be_a TestingSiteOnlyoffice::SiteDocSpaceSignUp
+  end
+
+  it '[Site Products] [DocsSpace] "Deploy on your own server" button works' do
+    expect(@docspace_page.click_deploy_on_your_own_server).to be_a TestingSiteOnlyoffice::SiteDocSpaceDownloadEnterprise
+  end
+
+  it '[Site Products] [DocsSpace] "Connectors - get it now" button works' do
+    expect(@docspace_page.click_connectors_get_it_now).to be_a TestingSiteOnlyoffice::SiteConnectorsOnlyoffice
+  end
+
+  it '[Site Products] [DocsSpace] "Download desktop" button works' do
+    expect(@docspace_page.click_download_pc_now).to be_a TestingSiteOnlyoffice::SiteGetOnlyofficeDesktopApps
+  end
+
+  it '[Site Products] [DocsSpace] "Install mobile" button works' do
+    expect(@docspace_page.click_mobile_install_now).to be_a TestingSiteOnlyoffice::SiteMobileApps
+  end
+end
