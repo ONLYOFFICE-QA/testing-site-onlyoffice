791fac3f36ebb8862d644f58b3745e9c6031eae6
Julia B <bubnovaum@gmail.com>
2024-04-05 16:07:58 +0400

    add new tests for docbuilder overview page

--- lib/testing_site_onlyoffice/for_developers/site_for_developers_doc_builder.rb
+++ lib/testing_site_onlyoffice/for_developers/site_for_developers_doc_builder.rb
@@ -6,8 +6,16 @@ module TestingSiteOnlyoffice
   class SiteForDevelopersDocBuilder
     include PageObject
     include SiteToolbar
+    include SiteDownloadHelper
 
+    link(:download_now_top, xpath: '//div[@class="dbl-header-button-area"]//a[@href="/download-builder.aspx"]')
     link(:code_download_link, xpath: '//a[contains(@class, "code_download_link")]')
+    link(:github_open_source, xpath: '//a[contains(@href, "github.com/ONLYOFFICE/DocumentBuilder")]')
+    link(:read_documentaion_top, xpath: '(//a[@class="dbldb_block_link" and @href="https://api.onlyoffice.com/docbuilder/basic"])[1]')
+    link(:read_documentaion_bottom, xpath: '(//a[@class="dbldb_block_link" and @href="https://api.onlyoffice.com/docbuilder/basic"])[2]')
+    link(:download_now_bottom, xpath: '//div[@class="dbldb_block_download_button"]//a[@href="/download-builder.aspx"]')
+    link(:docs_developer, xpath: '//p//a[contains(@href, "/developer-edition.aspx")]')
+    link(:buy_now, xpath: '//div[@class="dbldisc_block_contact_button"]//a[@href="/developer-edition-prices.aspx"]')
 
     def initialize(instance)
       super(instance.webdriver.driver)
@@ -19,6 +27,45 @@ module TestingSiteOnlyoffice
       @instance.webdriver.wait_until do
         @instance.webdriver.element_present?(code_download_link_element)
       end
+    end
+
+    features_xpaths = {
+      document_processing: '//div[contains(@class, "code_description_heading cdblock_1")]',
+      spreadsheet_creation: '//div[contains(@class, "code_description_heading cdblock_2")]',
+      presentation_creation: '//div[contains(@class, "code_description_heading cdblock_3")]',
+      form_building: '//div[contains(@class, "code_description_heading cdblock_6")]',
+      pdf_generating: '//div[contains(@class, "code_description_heading cdblock_5")]'
+    }
+
+    features_xpaths.each_with_index do |(feature, xpath), index|
+      define_method(:"click_#{feature}_and_download_file") do
+        @instance.webdriver.click_on_locator(xpath)
+        @instance.webdriver.click_on_locator("(//a[contains(@class, 'code_download_link')])[#{index + 1}]")
+      end
+    end
+
+    def click_download_now_top
+      download_now_top_element.click
+      SiteGetOnlyofficeDownloadDocBuilder.new(@instance)
+    end
+
+    def click_download_now_bottom
+      download_now_bottom_element.click
+      SiteGetOnlyofficeDownloadDocBuilder.new(@instance)
+    end
+
+    def click_docs_developer
+      docs_developer_element.click
+      SiteForDevelopersDocDevEdition.new(@instance)
+    end
+
+    def click_buy_now
+      buy_now_element.click
+      SitePriceDocsDeveloper.new(@instance)
+    end
+
+    def opened_file_url
+      @instance.webdriver.current_url
     end
   end
 end
