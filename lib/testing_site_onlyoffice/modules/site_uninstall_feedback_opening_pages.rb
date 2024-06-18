fe98c9968e852fa0745254cf99a50c16432932bc
Julia B <bubnovaum@gmail.com>
2024-06-18 19:12:53 +0300

    fix opening methods

--- lib/testing_site_onlyoffice/modules/site_uninstall_feedback_opening_pages.rb
+++ lib/testing_site_onlyoffice/modules/site_uninstall_feedback_opening_pages.rb
@@ -10,25 +10,25 @@ module TestingSiteOnlyoffice
   # These methods use the URLs of the test site because emails are not tested on the production environment
   module SiteUninstallFeedbackOpeningPages
     def open_install_canceled
-      url = config.server.end_with?('teamlab.info') ? "#{config.server}/install-canceled.aspx?Site_Testing=4testing" : "#{config.server}/install-canceled.aspx"
+      url = "#{config.server}/install-canceled.aspx"
       @instance.webdriver.open(url)
       SiteInstallCanceledPage.new(@instance)
     end
 
     def open_account_canceled
-      url = config.server.end_with?('teamlab.info') ? "#{config.server}/account-canceled.aspx?Site_Testing=4testing" : "#{config.server}/account-canceled.aspx"
+      url = "#{config.server}/account-canceled.aspx?"
       @instance.webdriver.open(url)
       SiteAccountCanceled.new(@instance)
     end
 
     def open_desktop_uninstalled
-      url = config.server.end_with?('teamlab.info') ? "#{config.server}/desktop-uninstalled.aspx?Site_Testing=4testing" : "#{config.server}/desktop-uninstalled.aspx"
+      url = "#{config.server}/desktop-uninstalled.aspx"
       @instance.webdriver.open(url)
       SiteDesktopUninstalled.new(@instance)
     end
 
     def open_registration_canceled
-      url = config.server.end_with?('teamlab.info') ? "#{config.server}/registration-canceled.aspx?Site_Testing=4testing" : "#{config.server}/registration-canceled.aspx"
+      url = "#{config.server}/registration-canceled.aspx"
       @instance.webdriver.open(url)
       SiteRegistrationCanceled.new(@instance)
     end
