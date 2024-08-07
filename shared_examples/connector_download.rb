8208ea3b98440288eb067ab489123a0e6ad51f26
Julia B <bubnovaum@gmail.com>
2024-08-07 11:03:52 +0300

    update title for box connector

--- shared_examples/connector_download.rb
+++ shared_examples/connector_download.rb
@@ -9,6 +9,9 @@ shared_examples_for 'connector_download' do |connectors_list|
         if connector.to_s == 'zoom'
           possible_titles = ['App Marketplace', 'ONLYOFFICE DocSpace']
           expect(possible_titles).to include(connectors_page.check_opened_page_title)
+        elsif connector.to_s == 'box'
+          possible_titles = ['ONLYOFFICE Docs | Box App Center', 'ONLYOFFICE Docs | Box App Center | Powered by Box']
+          expect(possible_titles).to include(connectors_page.check_opened_page_title)
         else
           github_title = TestingSiteOnlyoffice::SiteDownloadData.connectors_info[connector.to_s]['get_it_now']
           expect(connectors_page.check_opened_page_title_and_wait_until_not_empty).to eq(github_title)
