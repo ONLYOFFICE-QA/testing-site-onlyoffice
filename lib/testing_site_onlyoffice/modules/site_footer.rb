15b6e93e3668b63e4b781387c33576d281fda4c2
Julia B <bubnovaum@gmail.com>
2025-02-28 10:01:23 +0300

    fix robocop

--- lib/testing_site_onlyoffice/modules/site_footer.rb
+++ lib/testing_site_onlyoffice/modules/site_footer.rb
@@ -103,9 +103,7 @@ module TestingSiteOnlyoffice
     footer_links.each_key do |link|
       define_method(:"click_#{link}") do
         @instance.webdriver.click_on_locator(send(:"#{link}_element"))
-        if link == :fill_out_pdf_forms_online
-          @instance.webdriver.choose_tab(2)
-        end
+        @instance.webdriver.choose_tab(2) if link == :fill_out_pdf_forms_online
         footer_links[link].new(@instance)
       end
     end
