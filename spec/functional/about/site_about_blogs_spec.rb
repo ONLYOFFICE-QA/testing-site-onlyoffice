b848be00af4d2f6f040a6cdeae0666afe81e44df
Julia B <bubnovaum@gmail.com>
2025-03-31 11:26:36 +0300

    update blog test suites

--- spec/functional/about/site_about_blogs_spec.rb
+++ spec/functional/about/site_about_blogs_spec.rb
@@ -6,18 +6,15 @@ test_manager = TestingSiteOnlyoffice::TestManager.new(suite_name: File.basename(
 
 describe 'About blogs' do
   before do
-    site_home_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
-    @about_blogs_page = site_home_page.click_link_on_toolbar(:about_blog)
+    @site_home_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
   end
 
   after do |example|
     test_manager.add_result(example, @test)
     @test.webdriver.quit
   end
-
-  TestingSiteOnlyoffice::SiteData.download_desktop_apps.each do |apps|
-    it "[About blogs] Go to desktop #{apps}" do
-      expect(@about_blogs_page.go_to_desktop_apps(apps)).to be_a TestingSiteOnlyoffice::SiteGetOnlyofficeDesktopApps
+    it "[About] Go to blog}" do
+      about_blogs_page = @site_home_page.click_link_on_toolbar(:about_blog)
+      expect(about_blogs_page).to be_a TestingSiteOnlyoffice::SiteAboutBlog
     end
-  end
 end
