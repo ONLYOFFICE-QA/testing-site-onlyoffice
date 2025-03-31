b3c22c90d73decb45970ff837037d3c69a782ee1
Julia Bubnova <99170537+JuliaBuvx@users.noreply.github.com>
2025-03-31 11:41:30 +0300

    update blog test suites (#2421)
    * update blog test suites
    
    * lint fixes
    
    * lint fixes

--- spec/functional/about/site_about_blogs_spec.rb
+++ spec/functional/about/site_about_blogs_spec.rb
@@ -6,8 +6,7 @@ test_manager = TestingSiteOnlyoffice::TestManager.new(suite_name: File.basename(
 
 describe 'About blogs' do
   before do
-    site_home_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
-    @about_blogs_page = site_home_page.click_link_on_toolbar(:about_blog)
+    @site_home_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
   end
 
   after do |example|
@@ -15,9 +14,8 @@ describe 'About blogs' do
     @test.webdriver.quit
   end
 
-  TestingSiteOnlyoffice::SiteData.download_desktop_apps.each do |apps|
-    it "[About blogs] Go to desktop #{apps}" do
-      expect(@about_blogs_page.go_to_desktop_apps(apps)).to be_a TestingSiteOnlyoffice::SiteGetOnlyofficeDesktopApps
-    end
+  it '[About] Go to blog}' do
+    about_blogs_page = @site_home_page.click_link_on_toolbar(:about_blog)
+    expect(about_blogs_page).to be_a TestingSiteOnlyoffice::SiteAboutBlog
   end
 end
