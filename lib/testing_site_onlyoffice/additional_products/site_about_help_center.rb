89688e8d8d8f5c0e95fd42f0fbc6c9995816caff
Julia B <bubnovaum@gmail.com>
2025-04-01 11:39:39 +0300

    update locator for helpcenter

--- lib/testing_site_onlyoffice/additional_products/site_about_help_center.rb
+++ lib/testing_site_onlyoffice/additional_products/site_about_help_center.rb
@@ -10,7 +10,7 @@ module TestingSiteOnlyoffice
 
     include PageObject
 
-    div(:headers_menu, xpath: '//div[@class="left-menu-wrapper"]')
+    div(:headers_menu, xpath: '//div[@class="info-content-header"]')
 
     def initialize(instance)
       super(instance.webdriver.driver)
