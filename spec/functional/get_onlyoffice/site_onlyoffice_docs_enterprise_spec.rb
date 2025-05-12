00ff7df4bd65569145d6224ec350242973296750
Julia B <bubnovaum@gmail.com>
2025-05-12 21:25:01 +0300

    add parameter for hcaptcha pass

--- spec/functional/get_onlyoffice/site_onlyoffice_docs_enterprise_spec.rb
+++ spec/functional/get_onlyoffice/site_onlyoffice_docs_enterprise_spec.rb
@@ -8,6 +8,7 @@ describe 'Onlyoffice Docs Enterprise edition download' do
   before do
     site_home_page, @test = TestingSiteOnlyoffice::SiteHelper.new.open_page_teamlab_office(config)
     @download_commercial_page = site_home_page.click_link_on_toolbar(:download_docs_enterprise)
+    site_home_page.append_url_param
   end
 
   it_behaves_like 'commercial_installer_download', 'Docs_Enterprise',
